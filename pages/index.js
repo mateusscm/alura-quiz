517529dcd67c752c592ce75bd10d0ba4e9c8178b
mateus.m <mateus.m@juristecplus.com.br>
2021-02-08 21:55:05 -0300

    finalizando a aula, mas preciso modificar os dados

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,130 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import { Widget } from '../src/components/Widget';
-import GitHubCorner from '../src/components/GitHubCorner';
+import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizContainer from '../src/components/QuizContainer';
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
   const router = useRouter();
-  const [name, setName] = useState('');
+  const [name, setName] = React.useState('');
 
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          AluraQuiz -
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
-            <h1>#JavaScriptQuiz</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={(event) => {
-              event.preventDefault();
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
             }}
             >
-              <Input placeholder="Diz ai seu nome" value={name} onChange={(e) => setName(e.target.value)} />
-              <Button type="submit" disabled={name?.length === 0}>
-                Jogar
-                {' '}
-                {name}
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
         </Widget>
-        <Widget>
+
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
-            <h1>Quizes da galera</h1>
-            <p>lorem ipsum dolor sit amet...</p>
+            <h1>Quizes da Galera</h1>
+
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/mateusscm" />
     </QuizBackground>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
 
-export const Widget = styled.div`
+const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
   border: 1px solid ${({ theme }) => theme.colors.primary};
   background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
-
   h1, h2, h3 {
     font-size: 16px;
     font-weight: 700;
     line-height: 1;
     margin-bottom: 0;
   }
-
   p {
     font-size: 14px;
     font-weight: 400;
@@ -24,11 +22,11 @@ export const Widget = styled.div`
 
 Widget.Header = styled.header`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   padding: 18px 32px;
   background-color: ${({ theme }) => theme.colors.primary};
-
+  
   * {
     margin: 0;
   }
@@ -36,18 +34,15 @@ Widget.Header = styled.header`
 
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
-
   & > *:first-child {
     margin-top: 0;
   }
-
   & > *:last-child {
     margin-bottom: 0;
-
-    ul {
-      list-style: none;
-      padding: 0;
-    }
+  }
+  ul {
+    list-style: none;
+    padding: 0;
   }
 `;
 
@@ -68,3 +63,5 @@ Widget.Topic = styled.a`
     opacity: .5;
   }
 `;
+
+export default Widget;
